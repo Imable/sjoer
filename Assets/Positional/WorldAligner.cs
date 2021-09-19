8d833c7c54478b7af08336ddffebfd78c55c3a27
Abel <Abel.Beek@student.uib.no>
2021-09-19 11:29:52 +0200

    Calculated transformation vector from Unity coordinate system to TrueNorth (more plausible than before, will test later)

--- Assets/Positional/WorldAligner.cs
+++ Assets/Positional/WorldAligner.cs
@@ -10,8 +10,8 @@ namespace Assets.Positional
         public GameObject cloneThisObject;
         public Camera mainCamera;
 
-        // Difference between vessel bearing and Hololens bearing
-        private float bearingDiff = 0;
+        // Difference between vessel bearing (true north) and Hololens bearing (unity coordinates)
+        private Vector3 unityToTrueNorthRotation = Vector3.zero;
         private GPSInfoDTO lastGPSUpdate;
 
         private DataRetriever dataRetriever;
@@ -23,47 +23,66 @@ namespace Assets.Positional
         {
             dataRetriever = new DataRetriever(DataSources.GPSInfo);
             lastGPSUpdate = (GPSInfoDTO)dataRetriever.fetch();
-            updateAlignVesselAndHoloLens();
-            keyboard = TouchScreenKeyboard.Open("text to edit");    
+            unityToTrueNorthRotation = unitytoTrueNorth();
+
+            //updateAlignVesselAndHoloLens();
         }
 
         // Update is called once per frame
         void Update()
         {
-            //lastGPSUpdate = (GPSInfoDTO) dataRetriever.fetch();
-            //Debug.Log(mainCamera.transform.position);
-            
-            if (Input.GetKey(KeyCode.S))
+            lastGPSUpdate = (GPSInfoDTO)dataRetriever.fetch();
+
+
+            Debug.Log($"HoloForward: {mainCamera.transform.forward}");
+            Debug.Log($"HoloRotationAroundY: {mainCamera.transform.rotation.eulerAngles.y}");
+            Debug.Log($"UnityToTrueNorth: {unityToTrueNorthRotation}");
+
+
+
+            if (Input.GetKey(KeyCode.O))
             {
                 DataRetriever marineTrafficAIS = new DataRetriever(DataSources.MarineTrafficAIS);
-                MarineTrafficAISDTO mtAIS = (MarineTrafficAISDTO) marineTrafficAIS.fetch();
+                MarineTrafficAISDTO mtAIS = (MarineTrafficAISDTO)marineTrafficAIS.fetch();
                 Debug.Log(mtAIS.Identifier);
 
                 double x, y, z;
                 HelperClasses.GPSUtils.Instance.GeodeticToEnu(mtAIS.Latitude, mtAIS.Longitude, 0, lastGPSUpdate.Latitude, lastGPSUpdate.Longitude, 0, out x, out y, out z);
                 Debug.Log($"{x},{y},{z}");
 
-                Instantiate(cloneThisObject, mainCamera.transform.position + new Vector3((float)x, (float)y, (float)z), holoLensRelativeToTrueNorth(mainCamera.transform.rotation));
-                //Instantiate(cloneThisObject, mainCamera.transform.position + mainCamera.transform.forward * 2.0f, mainCamera.transform.rotation);
+                Instantiate(cloneThisObject, mainCamera.transform.position + new Vector3((float)x, (float)z, (float)y), Quaternion.Euler(unityToTrueNorthRotation));
             }
+                //    //Instantiate(cloneThisObject, mainCamera.transform.position + mainCamera.transform.forward * 2.0f, mainCamera.transform.rotation);
+                //}
 
-            if (Input.GetKey(KeyCode.A))
-            {
-                updateAlignVesselAndHoloLens();
-            }
+                //if (Input.GetKey(KeyCode.A))
+                //{
+                //    updateAlignVesselAndHoloLens();
+                //}
+        }
+
+
+        // The rotation that transforms the Unity north axis to True north
+        // This should only be executed when Hololens and vessel are aligned
+        // (and thus vessel compass information and Hololens direction match)
+        Vector3 unitytoTrueNorth()
+        {
+            return new Vector3(0, mainCamera.transform.rotation.eulerAngles.y
+                                    - (float) lastGPSUpdate.TrueCourse, 0);
         }
 
         // HoloLens should face in same direction as the vessel and then this function should be invoked
         // Now we have reference values of the direction the HoloLens is facing, compared to 'True North'
-        void updateAlignVesselAndHoloLens()
-        {
-            this.bearingDiff = (float)lastGPSUpdate.TrueCourse - mainCamera.transform.rotation.eulerAngles.y;
-        }
+        //void updateAlignVesselAndHoloLens()
+        //{
 
-        Quaternion holoLensRelativeToTrueNorth(Quaternion holoLensRotation)
-        {
-            return holoLensRotation *= Quaternion.Euler(Vector3.up * bearingDiff);
-        }
+        //    this.bearingDiff = new Vector3(0, (float)lastGPSUpdate.TrueCourse - mainCamera.transform.rotation.eulerAngles.y, 0);
+        //}
+
+        //Quaternion holoLensRelativeToTrueNorth(Quaternion holoLensRotation)
+        //{
+        //    return holoLensRotation *= Quaternion.Euler(Vector3.up * bearingDiff);
+        //}
     }
 
 }
